e2c073e3591db82caa78ee0682ce9790e7e21af6
H <hsl1991@live.com>
2018-07-19 11:27:08 +0800

    添加项目文件。

---
+++ nop.Cache.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nop.Cache.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CacheHelper.Insert("name", "张三");
+            string name = CacheHelper.Get<string>("name");
+            Console.WriteLine(name);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ nop.Cache/CacheHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nop.Cache
+{
+    public class CacheHelper
+    {
+        public static readonly string _RedisCachingConnectionString =
+            ConfigurationManager.AppSettings["RedisCachingConnectionString"];
+
+        private static readonly RedisCacheManager _RedisCacheManager;
+
+        static CacheHelper()
+        {
+            //创建缓存
+            if (!string.IsNullOrWhiteSpace(_RedisCachingConnectionString))
+            {
+                RedisConnectionWrapper RedisConnectionWrapper = new RedisConnectionWrapper(_RedisCachingConnectionString);
+                _RedisCacheManager = new RedisCacheManager(RedisConnectionWrapper);
+            }
+        }
+
+        /// <summary>
+        ///     创建缓存项的文件
+        /// </summary>
+        /// <param name="key">缓存Key</param>
+        /// <param name="obj">object对象</param>
+        public static void Insert(string key, object obj)
+        {
+            Insert(key, obj, 60);
+        }
+
+        /// <summary>
+        ///     移除缓存项的文件
+        /// </summary>
+        /// <param name="key">缓存Key</param>
+        public static void Remove(string key)
+        {
+            _RedisCacheManager.Remove(key);
+        }
+
+        /// <summary>
+        ///     创建缓存项过期
+        /// </summary>
+        /// <param name="key">缓存Key</param>
+        /// <param name="obj">object对象</param>
+        /// <param name="expires">过期时间(分钟)</param>
+        public static void Insert(string key, object obj, int expires)
+        {
+            _RedisCacheManager.Set(key, obj, expires);
+        }
+
+        /// <summary>
+        ///     获取缓存对象
+        /// </summary>
+        /// <param name="key">缓存Key</param>
+        /// <returns>object对象</returns>
+        public static object Get(string key)
+        {
+            if (string.IsNullOrEmpty(key)) return null;
+            return _RedisCacheManager.Get<object>(key);
+        }
+
+        /// <summary>
+        ///     获取缓存对象
+        /// </summary>
+        /// <typeparam name="T">T对象</typeparam>
+        /// <param name="key">缓存Key</param>
+        /// <returns></returns>
+        public static T Get<T>(string key)
+        {
+            return _RedisCacheManager.Get<T>(key);
+        }
+    }
+}
