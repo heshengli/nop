4e9ef6ebb4df6711e984bd93186a83d3674cc787
H <hsl1991@live.com>
2018-07-19 21:18:34 +0800

    ef

--- nop.EntityFrameWork/Model/StudentMap.cs
+++ nop.EntityFrameWork/Model/StudentMap.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 namespace nop.EntityFrameWork.Model
 {
     /// <summary>
-    /// 实体映射表
+    /// 实体映射表nop拓展
     /// </summary>
     public partial class StudentMap : NopEntityTypeConfiguration<Student>
     {
@@ -18,4 +19,17 @@ namespace nop.EntityFrameWork.Model
             this.Property(pa => pa.Name).IsRequired();
         }
     }
+
+    /// <summary>
+    /// 原生ef
+    /// </summary>
+    //public partial class StudentMap2 : EntityTypeConfiguration<Student>
+    //{
+    //    public StudentMap2()
+    //    {
+    //        this.ToTable("Student");
+    //        this.HasKey(pa => pa.Id);
+    //        this.Property(pa => pa.Name).IsRequired();
+    //    }
+    //}
 }
